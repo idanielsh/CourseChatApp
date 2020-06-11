f6689e4a2376a9da52abef33bdbba42b293c33ab
shoiche7 <d.shoichet@mail.utoronto.ca>
2020-06-11 15:10:51 -0500

    Added class user

--- flutter_app/test/widget_test.dart
+++ flutter_app/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:courses_app/main.dart';
+import 'file:///Q:/Documents/FlutterProjects/CourseChatApp/flutter_app/lib/UI/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
